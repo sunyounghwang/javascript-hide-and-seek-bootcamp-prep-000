79cc14a49ecc86a306ddcac2c64418f58eec1558
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-07-10 19:10:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target');
+}
+
+function increaseRankBy(n) {
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let grandNode = document.getElementById('grand-node');
+  let children = grandNode.querySelectorAll('*');
+  
+  for (let i = 0, l = children.length; i < l; i++) {
+  
+  if (i === l - 1) {
+      return children[i];
+    }
+  }
+}
